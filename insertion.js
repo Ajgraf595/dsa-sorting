e1da6f411e8842da4d69cd55431038866f2f471f
Your Name <your.email@example.com>
2025-10-27 12:49:27 -0700

    Add insertion sort algorithm

--- insertion.js
+++ insertion.js
@@ -1,3 +1,23 @@
-function insertionSort() {}
-
-module.exports = insertionSort;
+function insertionSort(arr) {
+    if (!Array.isArray(arr)) throw new TypeError('insertionSort expects an array');
+    
+    // Create a shallow copy so we don't mutate the input
+    const a = arr.slice();
+  
+    for (let i = 1; i < a.length; i++) {
+      const key = a[i];
+      let j = i - 1;
+  
+      // Move elements greater than key to one position ahead
+      while (j >= 0 && a[j] > key) {
+        a[j + 1] = a[j];
+        j--;
+      }
+      a[j + 1] = key;
+    }
+  
+    return a;
+  }
+  
+  module.exports = insertionSort;
+  
